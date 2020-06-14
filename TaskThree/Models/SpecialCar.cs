1394527c49f54ede7f788abd5c5cbcec09c6235a
Roman Epifanov <roman@softwave.co.il>
2020-06-14 16:06:54 +0300

    added task three

---
+++ TaskThree/Models/SpecialCar.cs
@@ -0,0 +1,16 @@
+﻿namespace TaksThree.Models
+{
+    class SpecialCar : Car
+    {
+        public string Kind { get; set; }
+        public string Weight { get; set; }
+        public string OverallDimensions { get; set; }
+
+        public SpecialCar(Car car, string kind, string weight, string overallDimensions) : base(car)
+        {
+            this.Kind = kind;
+            this.Weight = weight;
+            this.OverallDimensions = overallDimensions;
+        }
+    }
+}
---
+++ TaskThree/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using TaksThree.Controllers;
+using TaksThree.Models;
+
+namespace TaskThree
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            SellController sellController = new SellController();
+
+            sellController.SellCar(new Car("Ford Focus II", 2007, "245 000 руб", "Хэтчбек 5 дв.", "Россия"));
+            sellController.SellCar(new Car("Audi", 2011, "600 000 руб", "Хэтчбек 5 дв.", "Россия"));
+            sellController.SellCar(new Car("Daewoo nexia", 2011, "200 000 руб", "Хэтчбек 5 дв.", "Россия"));
+            sellController.PrintList();
+
+            Console.WriteLine("--------------------------------"); 
+            sellController.ModelOrderByAscending();
+            sellController.PrintList();
+
+            Console.WriteLine("--------------------------------");
+            sellController.ModelOrderByDescending();
+            sellController.PrintList();
+
+            Console.WriteLine("--------------------------------");
+            sellController.PriceOrderByAscending();
+            sellController.PrintList();
+
+            Console.WriteLine("--------------------------------");
+            sellController.PriceOrderByDescending();
+            sellController.PrintList();
+        }
+    }
+}
