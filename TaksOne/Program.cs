fe63c0298ecc3f8429f3b1ab7418c5da01361b49
Roman Epifanov <roman@softwave.co.il>
2020-06-14 16:07:21 +0300

    delete comments

--- TaksOne/Program.cs
+++ TaksOne/Program.cs
@@ -9,9 +9,6 @@ namespace TaksOne
         static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            //Описать свойства, конструктор, методы геттеры/сеттеры, перекрыть метод
-            //toString() для вывода полной информации об объекте в отформатированном
-            //виде.
             Car car = new Car("Ford Focus II", 2007, "245 000 руб", "Хэтчбек 5 дв.", "Россия");
             Console.WriteLine(car.ToString());
         }
